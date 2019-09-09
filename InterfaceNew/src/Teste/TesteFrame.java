2b65d7cedce3cca74df08a51a1b37619798bfad1
JohnnyMendes <42049673+JohnnyMendes@users.noreply.github.com>
2019-09-09 20:14:33 -0300

    Interface Message
    Códigos para imprimir determinados tipos de texto utilizando Java, operando comm NetBeans IDE 11.1

---
+++ InterfaceNew/src/Teste/TesteFrame.java
@@ -0,0 +1,18 @@
+
+package Teste;
+
+import javax.swing.JFrame;
+
+
+public class TesteFrame {
+
+   
+    public static void main(String[] args) {
+        JFrame fr=new JFrame("Exemplo");
+        fr.setSize(400, 400);
+        fr.setVisible(true);
+        
+       
+    }
+    
+}
---
+++ InterfaceNew/src/Teste/TesteShowMessageQuestion.java
@@ -0,0 +1,15 @@
+
+package Teste;
+
+import javax.swing.JOptionPane;
+
+
+public class TesteShowMessageQuestion {
+    public static void main(String[] args){
+    JOptionPane.showMessageDialog(null,
+             "Teste dialogo QUESTION_MESSAGE",
+             "Mensagem de questionamento",
+             JOptionPane.QUESTION_MESSAGE);
+    }
+
+}
